fd6d9d45243714f9d5b9168a3ecef6f0cfa23653
nickmoreton <nick.moreton@torchbox.com>
2024-09-02 11:58:01 +0100

    Initial styling

---
+++ longclaw_bakery/static/longclaw_bakery.js
@@ -0,0 +1,2 @@
+(()=>{function o(){console.log("imported")}var e=class{constructor(){this.message="Hello, world!"}say(){console.log(this.message)}},s=new e;s.say();o();})();
+//# sourceMappingURL=longclaw_bakery.js.map
